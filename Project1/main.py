c71c609dca454f2f35a2201876889b03746b62e3
kajetan <kajetanzdanowicz@wp.pl>
2021-04-02 18:55:19 +0200

    chunk reading from image, chunk processor class

--- Project1/main.py
+++ Project1/main.py
@@ -1,6 +1,12 @@
 from tkinter import Tk, Label
+from chunk_processor import PNGChunkProcessor
 
 def main():
+    img_source = open('images/bird.png', 'rb')
+    chunk_processor = PNGChunkProcessor()
+    chunk_processor.save_chunks(img_source)
+    chunk_processor.print_chunks_types()
+
     root = Tk()
     label = Label(root, text="Hello world")
     label.pack()
