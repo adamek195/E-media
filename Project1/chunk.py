1bd956c4e10c9060a9996a05b482c6529bb79ca6
kajetan <kajetanzdanowicz@wp.pl>
2021-04-02 19:23:04 +0200

    chunk class added, some features from chunk processor moved there

---
+++ Project1/chunk.py
@@ -0,0 +1,17 @@
+import zlib
+import struct
+
+
+class Chunk:
+
+    def __init__(self, img):
+        self.chunk_length, self.chunk_type = struct.unpack('>I4s', img.read(8))
+        self.chunk_data = img.read(self.chunk_length)
+        self.chunk_crc, = struct.unpack('>I', img.read(4))
+        chunk_actual_crc = zlib.crc32(self.chunk_data, 
+                            zlib.crc32(struct.pack('>4s', self.chunk_type)))
+        if self.chunk_crc != chunk_actual_crc:
+            raise Exception('invalid chunk sum')
+
+
+
--- Project1/chunk_processor.py
+++ Project1/chunk_processor.py
@@ -1,35 +1,23 @@
-import struct
-import zlib
-
+from chunk import Chunk
 
 class PNGChunkProcessor:
 
     def __init__(self):
         self.chunks = []
 
-    def validate_png(self, img):
+    @classmethod
+    def validate_png(cls, img):
         png_signature = b'\x89PNG\r\n\x1a\n'
         if img.read(len(png_signature)) != png_signature:
             raise Exception('Invalid PNG Signature')
 
-    def read_chunk(self, img):
-        chunk_length, chunk_type = struct.unpack('>I4s', img.read(8))
-        chunk_data = img.read(chunk_length)
-        chunk_crc, = struct.unpack('>I', img.read(4))
-        chunk_actual_crc = zlib.crc32(chunk_data, zlib.crc32(struct.pack('>4s', chunk_type)))
-        if chunk_crc != chunk_actual_crc:
-            raise Exception('invalid chunk sum')
-        return chunk_type, chunk_data
-
     def save_chunks(self, img):
         self.validate_png(img)
         while True:
-            chunk_type, chunk_data = self.read_chunk(img)
-            self.chunks.append((chunk_type, chunk_data))
-            if chunk_type == b'IEND':
+            new_chunk = Chunk(img)
+            self.chunks.append(new_chunk)
+            if new_chunk.chunk_type == b'IEND':
                 break
-    
+
     def print_chunks_types(self):
-        print([chunk[0] for chunk in self.chunks])
-
-
+        print([chunk.chunk_type for chunk in self.chunks])
