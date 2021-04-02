00a6c7b34f1d0e500c31ee215ff6f9f079042d1e
kajetan <kajetanzdanowicz@wp.pl>
2021-04-02 19:25:11 +0200

    chunk.py cleanup

--- Project1/chunk.py
+++ Project1/chunk.py
@@ -8,10 +8,7 @@ class Chunk:
         self.chunk_length, self.chunk_type = struct.unpack('>I4s', img.read(8))
         self.chunk_data = img.read(self.chunk_length)
         self.chunk_crc, = struct.unpack('>I', img.read(4))
-        chunk_actual_crc = zlib.crc32(self.chunk_data, 
+        chunk_actual_crc = zlib.crc32(self.chunk_data,
                             zlib.crc32(struct.pack('>4s', self.chunk_type)))
         if self.chunk_crc != chunk_actual_crc:
             raise Exception('invalid chunk sum')
-
-
-
