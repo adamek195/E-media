e5188a8e322dd08a33b1afc1b16d3a0953f80c3d
kajetan <kajetanzdanowicz@wp.pl>
2021-04-03 00:29:49 +0200

    from now processed image is not hardcoded, can be chosen from window

--- Project1/main.py
+++ Project1/main.py
@@ -1,26 +1,28 @@
 from tkinter import Tk, Label, Button
+from tkinter.filedialog import askopenfilename
 from chunk_processor import PNGChunkProcessor
 
 
 root = Tk()
-chunk_processor = PNGChunkProcessor()
 
 
-def fetch_chunks():
+def choose_photo():
+    chunk_processor = PNGChunkProcessor()
     query = chunk_processor.return_chunks_names_query()
-    label = Label(root, text=query)
-    label.grid(row=3, column=0)
+    img_path = askopenfilename()
+    label_chunks = Label(root, text=query)
+    label_chunks.grid(row=3, column=0)
+    img_source = open(img_path, 'rb')
+    chunk_processor.save_chunks(img_source)
+    chunk_processor.print_chunks_types()
 
 
 def main():
-    img_source = open('images/bird.png', 'rb')
-    chunk_processor.save_chunks(img_source)
-    chunk_processor.print_chunks_types()
 
     root.title("PNG reader")
     root.geometry("400x400")
 
-    fetch_btn = Button(root, text="Fetch chunks", command=fetch_chunks)
+    fetch_btn = Button(root, text="Load photo", command=choose_photo)
     fetch_btn.grid(row=1, column=0, columnspan=1, pady=10, padx=10, ipadx=130)
 
     root.mainloop()
