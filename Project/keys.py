1d61dbd9364a3984bd8c54b2ac8c0622b38d8398
adamek195 <248955@student.pwr.edu.pl>
2021-05-17 13:52:56 +0200

    generate private key

--- Project/keys.py
+++ Project/keys.py
@@ -10,30 +10,63 @@ class Keys:
         self.euler_function = (self.p - 1)*(self.q - 1)
         self.n = self.p * self.q
 
-
     def gcd_euklides(self, a, b):
         while(b):
             a, b = b, a % b
 
         return a
 
+    def mod_inverse_euklides(self, a, m):
+        tmp_m = m
+        y = 0
+        x = 1
+
+        if (m == 1):
+            return 0
+
+        while (a > 1):
+            q = a // m
+            t = m
+
+            m = a % m
+            a = t
+            t = y
+
+            y = x - q * y
+            x = t
+
+        if (x < 0):
+            x = x + tmp_m
+
+        return x
+
     def generate_public_key(self):
         while True:
-            e = getrandbits(self.key_size)
-            if self.gcd_euklides(e, (self.p - 1) * (self.q - 1)) == 1:
+            self.e = getrandbits(self.key_size)
+            if self.gcd_euklides(self.e, self.euler_function) == 1:
                 break
 
-        if 1 > e or e > self.n:
+        if 1 > self.e or self.e > self.n:
             self.generate_public_key()
-        if e % 2 == 0:
+        if self.e % 2 == 0:
             self.generate_public_key()
 
-        public_key = {"e": e, "n": self.n}
-
+        public_key = {"e": self.e, "n": self.n}
         return public_key
+
+    def generate_private_key(self):
+        self.d = self.mod_inverse_euklides(self.e, self.euler_function)
+        print(self.d)
+
+        private_key = {"d": self.d, "n": self.n}
+        return private_key
+
 
 keys = Keys()
 public_key = keys.generate_public_key()
 print(public_key)
+print("\n\n\n\n")
+private_key = keys.generate_private_key()
+print(private_key)
 
 
